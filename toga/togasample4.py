b5ce10686ab4a911ee44199a19c4b92e0c08207e
Stanton Xu <xjiefeng@gmail.com>
2018-05-02 22:19:12 -0700

    Project structure change

---
+++ toga/togasample4.py
@@ -0,0 +1,45 @@
+import toga
+from toga.style.pack import *
+
+
+class Graze(toga.App):
+    def startup(self):
+        self.main_window = toga.MainWindow(title=self.name)
+
+        self.webview = toga.WebView(style=Pack(flex=1))
+        self.url_input = toga.TextInput(
+            initial='https://github.com/',
+            style=Pack(flex=1, padding=5)
+        )
+
+        box = toga.Box(
+            children=[
+                toga.Box(
+                    children=[
+                        self.url_input,
+                        toga.Button('Go', on_press=self.load_page, style=Pack(width=50, padding_right=5)),
+                    ],
+                    style=Pack(
+                        direction=COLUMN
+                    )
+                ),
+                self.webview,
+            ],
+            style=Pack(
+                direction=COLUMN
+            )
+        )
+
+        self.main_window.content = box
+        self.main_window.show()
+
+    def load_page(self, widget):
+        self.webview.url = self.url_input.value
+
+
+def main():
+    return Graze('Graze', 'org.pybee.graze')
+
+
+if __name__ == '__main__':
+    main().main_loop()
