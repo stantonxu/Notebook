2c04ddf0ffa8b6d08a0dfd2ea015bb75e2aceb81
Stanton Xu <xjiefeng@gmail.com>
2018-05-04 17:31:45 -0700

    Notebook implementation 0504

---
+++ notebook.py
@@ -0,0 +1,58 @@
+# using toga as the GUI framework
+import os
+
+import toga
+from toga.style.pack import *
+
+
+class Notebook(toga.App):
+    def action_saveas(self, widget):
+        self.main_window.save_file_dialog('Save as...', 'notebook1.txt')
+
+    def startup(self):
+        self.main_window = toga.MainWindow(title=self.name)
+
+        # box = toga.Box(
+        #     children=[
+        #         toga.MultilineTextInput(id='input1')
+        #     ],
+        #     style=Pack(direction=COLUMN)
+        # )
+
+        self.box = toga.Box('box1')
+        self.textinput = toga.MultilineTextInput('input1', style=Pack(flex=1))
+
+        # load file here
+        with open('notebook1.txt') as f:
+            self.textinput.value = f.read()
+        f.close()
+
+        self.box.add(self.textinput)
+
+        cmd_savaas = toga.Command(
+            # action=self.action_saveas,
+            action=self.save_file,
+            label='Save as...',
+            tooltip='Save to local file'
+        )
+
+        self.main_window.content = self.box
+        self.main_window.toolbar.add(cmd_savaas)
+        self.main_window.show()
+
+    def save_file(self, widget):
+        # notebook1.txt = textinput.value
+        f = open('notebook1.txt', 'w')
+        f.write(self.textinput.value)
+        f.close()
+
+    def exit(self):
+        self.save_file()
+
+
+def main():
+    return Notebook('Notebook', 'org.pybee.notebook')
+
+
+if __name__ == '__main__':
+    main().main_loop()
