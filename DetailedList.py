e582d693d3684b2cd6ee684368a2ef56b1ba24ec
Stanton Xu <xjiefeng@gmail.com>
2018-06-22 10:23:02 -0700

    DetailedList practice

--- DetailedList.py
+++ DetailedList.py
@@ -11,12 +11,16 @@ class MyDetailedList(toga.App):
         self.main_window = toga.MainWindow(title=self.name)
 
         self.box = toga.Box('box1')
-        self.detailedlist = toga.DetailedList('list1', style=Pack(flex=1))
+        data1 = ['Item 0', 'Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7']
+        self.detailedlist = toga.DetailedList(data=data1, on_select=self.selection_handler, style=Pack(flex=1))
 
         self.box.add(self.detailedlist)
 
         self.main_window.content = self.box
         self.main_window.show()
+
+    def selection_handler(widget, selection):
+        print('Row {} of widget {} was selected.'.format(selection, widget))
 
 
 def main():
