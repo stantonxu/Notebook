d5a803b14bbe7fba37ef7a278c6a43a6a7d8326e
Stanton Xu <xjiefeng@gmail.com>
2018-10-12 09:02:14 -0700

    change on Oct, test

--- notebook.py
+++ notebook.py
@@ -2,7 +2,7 @@
 import os
 
 import toga
-from toga.style.pack import *
+# from toga.style.pack import *
 
 
 class Notebook(toga.App):
@@ -11,7 +11,8 @@ class Notebook(toga.App):
         self.main_window = toga.MainWindow(title=self.name)
 
         self.box = toga.Box('box1')
-        self.textinput = toga.MultilineTextInput('input1', style=Pack(flex=1))
+        # self.textinput = toga.MultilineTextInput('input1', style=Pack(flex=1))
+        self.textinput = toga.MultilineTextInput('input1', style=None)
 
         # load file here
         # Need to deal with the file path later
@@ -28,7 +29,7 @@ class Notebook(toga.App):
     def save_file(self, widget, **kwargs):
         # notebook1.txt = textinput.value
         f = open('notebook1.txt', 'w')
-        f.write(self.textinput.value)
+        f.write(str(self.textinput.value))
         f.close()
 
 
